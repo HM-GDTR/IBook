085b90ff0a8ffe4a69281e9d755c7b37baafa304
HM-GDTR <572579046@qq.com>
2024-01-10 16:03:44 +0800

    修复bug: 发送私聊后发送框不清空

--- app/src/main/java/com/rainbowx/finalwork/Activities/ChatActivity.java
+++ app/src/main/java/com/rainbowx/finalwork/Activities/ChatActivity.java
@@ -2,7 +2,6 @@ package com.rainbowx.finalwork.Activities;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -11,12 +10,10 @@ import com.alibaba.fastjson2.JSONArray;
 import com.alibaba.fastjson2.JSONObject;
 import com.rainbowx.finalwork.Adapter.ChatAdapter;
 import com.rainbowx.finalwork.Bean.ChatBean;
-import com.rainbowx.finalwork.Bean.FriendBean;
 import com.rainbowx.finalwork.R;
 import com.rainbowx.finalwork.Services.Chat;
 import com.rainbowx.finalwork.Services.User;
 import com.rainbowx.finalwork.Utils.Error;
-import com.rainbowx.finalwork.Utils.WebCallBack;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -47,82 +44,67 @@ public class ChatActivity extends AppCompatActivity {
         uid = intent.getIntExtra("uid", -1);
         if(uid < 0) return;
 
-        User.findByUid(uid, new WebCallBack<JSONObject>() {
-            @Override
-            public void onHandling(boolean is_success, int code, JSONObject responseBody, String reason, Throwable tr) {
-                if(is_success && code == 0) {
-                    other = User.parse(responseBody);
-                    Chat.history(uid, new WebCallBack<JSONObject>() {
-                                @Override
-                                public void onHandling(boolean is_success, int code, JSONObject responseBody, String reason, Throwable tr) {
-                                    List<ChatBean> data = new ArrayList<>();
-                                    if(is_success && code == 0){
-                                        JSONArray arr = responseBody.getJSONArray("res");
-                                        int len = arr.size();
-                                        for(int i = 0; i < len; i++) {
-                                            JSONObject obj = arr.getJSONObject(i);
-                                            Chat chat = Chat.parse(obj);
-                                            data.add(new ChatBean(chat.order,chat.cid,chat.other,chat.content));
-                                        }
-                                    }
-                                    adapter = new ChatAdapter(ChatActivity.this, data);
+        User.findByUid(uid, (is_success, code, responseBody, reason, tr) -> {
+            if(is_success && code == 0) {
+                other = User.parse(responseBody);
+                Chat.history(uid, (is_success1, code1, responseBody1, reason1, tr1) -> {
+                    List<ChatBean> data = new ArrayList<>();
+                    if(is_success1 && code1 == 0){
+                        JSONArray arr = responseBody1.getJSONArray("res");
+                        int len = arr.size();
+                        for(int i = 0; i < len; i++) {
+                            JSONObject obj = arr.getJSONObject(i);
+                            Chat chat = Chat.parse(obj);
+                            data.add(new ChatBean(chat.order,chat.cid,chat.other,chat.content));
+                        }
+                    }
+                    adapter = new ChatAdapter(ChatActivity.this, data);
 
-                                    adapter.setOtherUser(other);
-                                    StaggeredGridLayoutManager st = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
-                                    chatView.setLayoutManager(st);
-                                    chatView.setAdapter(adapter);
-                                }
-                            }
-                    );
-                }
+                    adapter.setOtherUser(other);
+                    StaggeredGridLayoutManager st = new StaggeredGridLayoutManager(1,StaggeredGridLayoutManager.VERTICAL);
+                    chatView.setLayoutManager(st);
+                    chatView.setAdapter(adapter);
+                });
             }
         });
 
-        submit.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                String msg = editText.getText().toString();
-                Chat.send(uid, msg, new WebCallBack<JSONObject>() {
-                    @Override
-                    public void onHandling(boolean is_success, int code, JSONObject responseBody, String reason, Throwable tr) {
-                        if(is_success && (code == 0)) {
-                            refresh();
-                        }
-                        else {
-                            Toast toast;
-                            if(reason == null || reason.isEmpty())
-                                toast = Toast.makeText(ChatActivity.this, "发送失败: "+ Error.getLastError(), Toast.LENGTH_SHORT);
-                            else
-                                toast = Toast.makeText(ChatActivity.this, "发送失败: "+ reason, Toast.LENGTH_SHORT);
-                            toast.show();
-                        }
-                    }
-                });
-            }
+        submit.setOnClickListener(v -> {
+            String msg = editText.getText().toString();
+            Chat.send(uid, msg, (is_success, code, responseBody, reason, tr) -> {
+                if(is_success && (code == 0)) {
+                    refresh();
+                    editText.setText("");
+                }
+                else {
+                    Toast toast;
+                    if(reason == null || reason.isEmpty())
+                        toast = Toast.makeText(ChatActivity.this, "发送失败: "+ Error.getLastError(), Toast.LENGTH_SHORT);
+                    else
+                        toast = Toast.makeText(ChatActivity.this, "发送失败: "+ reason, Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            });
         });
     }
 
     protected void refresh() {
-        Chat.history(uid, new WebCallBack<JSONObject>() {
-            @Override
-            public void onHandling(boolean is_success, int code, JSONObject responseBody, String reason, Throwable tr) {
-                List<ChatBean> data = new ArrayList<>();
-                if(is_success && code == 0){
-                    JSONArray arr = responseBody.getJSONArray("res");
-                    int len = arr.size();
-                    for(int i = 0; i < len; i++) {
-                        JSONObject obj = arr.getJSONObject(i);
-                        Chat chat = Chat.parse(obj);
-                        data.add(new ChatBean(chat.order,chat.cid,chat.other,chat.content));
-                    }
+        Chat.history(uid, (is_success, code, responseBody, reason, tr) -> {
+            List<ChatBean> data = new ArrayList<>();
+            if(is_success && code == 0){
+                JSONArray arr = responseBody.getJSONArray("res");
+                int len = arr.size();
+                for(int i = 0; i < len; i++) {
+                    JSONObject obj = arr.getJSONObject(i);
+                    Chat chat = Chat.parse(obj);
+                    data.add(new ChatBean(chat.order,chat.cid,chat.other,chat.content));
                 }
-                else {
-                    data.add(new ChatBean(1, 0, 0, "test content"));
-                    data.add(new ChatBean(-1, 0, 0, "test content1"));
-                }
-                adapter.setChatList(data);
-                adapter.notifyDataSetChanged();
             }
+            else {
+                data.add(new ChatBean(1, 0, 0, "test content"));
+                data.add(new ChatBean(-1, 0, 0, "test content1"));
+            }
+            adapter.setChatList(data);
+            adapter.notifyDataSetChanged();
         });
     }
 
